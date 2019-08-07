8d4fccb4f2cd3b622b0e17e8f5bf267a9f87b3c3
blessedbyjobs <alrbkv@gmail.com>
2019-08-07 14:58:56 +0300

    Fix CD

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ..
+GRADLE="./gradlew"
+$GRADLE assembleRelease
+$GRADLE crashlyticsUploadDistributionRelease
+RELEASEFILE="${CIRCLE_ARTIFACTS}/habrcicdcat-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
+cp ./app/build/outputs/apk/app-release.apk ${RELEASEFILE}
